82a550e90212f6d7139f8ae1b553a7ab1c4a017a
Trent Shailer <trent.shailer@gmail.com>
2025-07-08 17:38:34 +1200

    feat: port from identity service

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+interface Uint8Array<TArrayBuffer extends ArrayBufferLike> {
+  toBase64(options?: { alphabet?: "base64" | "base64url"; omitPadding?: boolean }): string;
+}
+
+interface Uint8ArrayConstructor {
+  fromBase64(
+    string: string,
+    options?: {
+      alphabet?: "base64" | "base64url";
+      lastChunkHandling?: "loose" | "strict" | "stop-before-partial";
+    },
+  ): Uint8Array;
+}
